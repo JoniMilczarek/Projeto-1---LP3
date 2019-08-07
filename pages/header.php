41fa066d930ef3f64e12a1f7fa00314bf30771f6
Ronaldo Manfredini <ronald677br@gmail.com>
2019-08-07 19:39:22 -0300

    init php

---
+++ pages/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Talento Moto Racing</title>
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/sobre.css">
+    <link rel="stylesheet" href="../styles/navbar.css">
+    <link rel="stylesheet" href="../styles/footer.css">
+    <link rel="shortcut icon" href="../components/favico.ico">
+    <link href="https://fonts.googleapis.com/css?family=ABeeZee&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="nav-body">
+      <a class="nav-title" href="#">Talento moto racing</a>
+      <div class="nav-links">
+        <ul>
+          <li><a href="">Sobre</a></li>
+          <li><a href="#">Rona</a></li>
+          <li><a href="#">Lindo</a></li>
+        </ul>
+      </div>
+    </nav>
