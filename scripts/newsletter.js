83bd5b4b41b31792b2215a8212b2bffab7bdb918
Ronaldo Manfredini <ronald677br@gmail.com>
2019-08-22 20:27:09 -0300

    newsletter

---
+++ scripts/newsletter.js
@@ -0,0 +1,17 @@
+function validateMail() {
+	var emailInput = document.getElementById("mail");
+	var value = emailInput.value;
+	emailInput.parentElement.classList.remove("invalid-field");
+	emailInput.parentElement.classList.remove("success");
+
+	var hasAt = value.indexOf('@') != -1;
+	var hasDotBeforeAt = value.indexOf('.') != -1 && value.indexOf('.') > value.indexOf('@');
+	var hasSomethingBeforeDot = value.indexOf('.') < value.length - 1;
+
+	if (value.length > 0 && hasAt && hasDotBeforeAt && hasSomethingBeforeDot) {
+		return emailInput.classList.add("success");
+	}
+
+	return emailInput.parentElement.classList.add("invalid-field");
+
+}
