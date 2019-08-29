6ccdb7b1d0115c5f5898dd98d05a475e46131ad6
Matheus Menegat <matheusmenegat@hotmail.com>
2019-08-29 19:57:22 -0300

    validando email login

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,27 @@
 var email = document.querySelector("input[name='email']");
-var senha = document.querySelector("input[name='senha']");
+var buttoSubmit = document.querySelector("button[name='button']");
 
-email.addEventListener('keyup', () => {
 
-  let expReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  let testaER = expReg.test(email.value);
+email.addEventListener('blur', () => {
+
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  var passou = false;
+
+	if (re.test(email.value)) {
+
+    email.classList.add("correto");
+    passou = true;
+	} else {
+
+    email.classList.add("incorreto");
+    passou = false;
+	}
+
+  if(email.value == 0) {
+
+    email.classList.remove("correto");
+  	email.classList.remove("incorreto");
+  }
 
 });
